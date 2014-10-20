c37982f66ddfdaf3152ad80379d9c310db404b19
Matthew Pflueger <matthew.pflueger@gmail.com>
2014-10-20 15:22:50 -0400

    Finished Chapter2 and added .psci file

--- src/Chapter2.purs
+++ src/Chapter2.purs
@@ -3,7 +3,9 @@ module Chapter2 where
 import Math
 import Debug.Trace
 
+circleArea radius = Math.pi * (radius * radius)
 diagonal w h = sqrt (w * w + h * h)
 
-main = print (diagonal 3 4)
+main = print (circleArea 3.4)
+-- main = print (diagonal 3 4)
 -- main = trace "Hello World!"
