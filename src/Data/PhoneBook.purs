3749d5c34384117b1013b557d9cf9317c6485e1b
Matthew Pflueger <matthew.pflueger@gmail.com>
2014-10-21 12:12:14 -0400

    Updated .psci, started Chapter3 with PhoneBook.purs

---
+++ src/Data/PhoneBook.purs
@@ -0,0 +1,9 @@
+module Data.PhoneBook where
+
+import Data.List
+import Data.Maybe
+import Control.Plus (empty)
+
+type Entry = { firstName :: String, lastName :: String, phone :: String }
+
+type PhoneBook = List Entry
