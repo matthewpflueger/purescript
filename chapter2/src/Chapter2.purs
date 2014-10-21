47ad52c86ccc842cd9edbb80621949264eda36b7
Matthew Pflueger <matthew.pflueger@gmail.com>
2014-10-21 16:14:33 -0400

    Restructure the code to be like the books code

--- chapter2/src/Chapter2.purs
+++ chapter2/src/Chapter2.purs
@@ -6,6 +6,6 @@ import Debug.Trace
 circleArea radius = Math.pi * (radius * radius)
 diagonal w h = sqrt (w * w + h * h)
 
-main = print (circleArea 3.4)
+-- main = print (circleArea 3.4)
 -- main = print (diagonal 3 4)
 -- main = trace "Hello World!"
